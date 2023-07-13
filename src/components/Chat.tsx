1e656029f91daf002a12ad48f181184ccb51840d
Daniel <abraodaniel@hotmail.com>
2023-07-13 14:35:34 -0300

    adjusting scroll and title

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -5,6 +5,7 @@ import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { useChat } from 'ai/react'
+import { ScrollArea } from "./ui/scroll-area"
 
 export function Chat() {
   const { messages, input, handleInputChange, handleSubmit} = useChat({
@@ -13,15 +14,16 @@ export function Chat() {
 
 
   return (
-    <Card className="w-[600px] h-[700px] grid grid-rows-[min-content_1fr_min-content]">
+    <Card className="w-[600px]">
         <CardHeader>
           <CardTitle>Chat AI</CardTitle>
           <CardDescription>Creating a chat bot</CardDescription>
         </CardHeader>
-        <CardContent className="space-y-4">
+        <CardContent>
+          <ScrollArea className="h-[600px] w-full pr-4">
           { messages.map(message => {
             return (
-              <div key={message.id} className="flex gap-3 text-slate-600 text-sm">
+              <div key={message.id} className="flex gap-3 text-slate-600 text-sm mb-4">
                 {message.role === 'user' && (
                   <Avatar>
                   <AvatarFallback>DA</AvatarFallback>
@@ -36,12 +38,13 @@ export function Chat() {
                 </Avatar>
                 )}
                 <p className="leading-relaxed">
-                  <span className="block font-bold text-slate-800">{message.role === 'user' ? "User" : "AI"}:</span>
+                  <span className="block font-bold text-slate-800">{message.role === 'user' ? "User" : "AI"} :</span>
                   {message.content}
                 </p>
               </div>
             )
           })}
+          </ScrollArea>
         </CardContent>
         <CardFooter >
           <form className="w-full flex gap-2" onSubmit={handleSubmit}>
